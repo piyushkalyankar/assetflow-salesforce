99afd8c5fafd92d7b301f3aca1c9ff41dc323263
Piyush Kalyankar <105622984+piyushkalyankar@users.noreply.github.com>
2026-07-14 12:16:41 +0530

    Add asset-request LWC and Apex getters
    Introduce a full asset request LWC form (HTML, JS) and expose it on Record Page. Wire cacheable Apex getters to populate employee and asset comboboxes. Add pass-through selectors/service methods (getActiveEmployees, getAvailableAssets) and small cleanup (remove empty constructor) in Apex. Enables frontend to load available assets and active employees for asset requests.

--- force-app/main/default/lwc/assetRequestForm/assetRequestForm.js
+++ force-app/main/default/lwc/assetRequestForm/assetRequestForm.js
@@ -1,3 +1,57 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getActiveEmployees from '@salesforce/apex/AssetController.getActiveEmployees';
+import getAvailableAssets from '@salesforce/apex/AssetController.getAvailableAssets';
 
-export default class AssetRequestForm extends LightningElement {}
+
+export default class AssetRequestForm extends LightningElement {
+    assetRequest = {
+        employeeId: null,
+        assetId: null,
+        priority: '',
+        reason: ''
+    };
+    employeeOptions=[];
+    assetOptions=[];
+    priorityOptions = [
+        { label: 'High', value: 'High' },
+        { label: 'Medium', value: 'Medium' },
+        { label: 'Low', value: 'Low' }
+    ];
+
+    @wire(getActiveEmployees)
+    wiredEmployees({ data, error }) {
+        if (data) {
+           this.employeeOptions = data.map((employee) => {
+            return {
+                label: employee.Name,
+                value: employee.Id
+            }
+           })
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    @wire(getAvailableAssets)
+    wiredAssets({data , error}){
+        if(data){
+            this.assetOptions = data.map((asset) => {
+                return {
+                    label:asset.Name,
+                    value:asset.Id
+                }
+            })
+        }else{
+            console.error(error);
+        }
+    }
+
+    handleChange(event){
+        this.assetRequest[event.target.name] = event.detail.value;
+    }
+
+    handleSubmit(){
+
+    }
+
+}
